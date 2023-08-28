6ba53fe272d5dc93ecaba1ecadb59442396cd323
Washx23 <5731@holbertonstudens.com>
2023-08-28 15:37:22 -0700

    3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a floating-point number.
+
+    Args:
+        n (float): The input floating-point number.
+
+    Returns:
+        str: The string representation of the input number.
+    """
+    return str(n)
