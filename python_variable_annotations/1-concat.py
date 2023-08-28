065a8a0f749c25e4d2f26a3d6706ec2ab3379975
Washx23 <5731@holbertonstudens.com>
2023-08-28 15:30:28 -0700

    1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the resulting string.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string of str1 and str2.
+    """
+    return str1 + str2
