15048a6dc657ff0af12ec84dbf885b0829b21f7d
Washx23 <5731@holbertonstudents.com>
2023-08-30 07:08:42 -0700

    1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,13 +4,15 @@
 
 import asyncio
 import random
+import heapq
 from typing import List
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Asynchronous coroutine that waits for a random
-    delay between 0 and max_delay (inclusive) seconds.
+    Asynchronous coroutine that waits for a
+    random delay between 0
+    and max_delay (inclusive) seconds.
     Returns the waited delay.
     """
     delay = random.uniform(0, max_delay)
@@ -20,22 +22,26 @@ async def wait_random(max_delay: int = 10) -> float:
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
-    Asynchronous routine that spawns
-    wait_random n times with specified max_delay.
-    Returns a list of all the delays in ascending order.
+    Asynchronous routine that spawns wait_random
+    n times with specified max_delay.
+    Returns a list of all
+    the delays in ascending order.
     """
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
-    delays.sort()
-    return delays
+
+    heapq.heapify(delays)
+    sorted_delays = [heapq.heappop(delays) for _ in range(len(delays))]
+
+    return sorted_delays
 
 
 async def main():
-    """module"""
     n = 5
     max_delay = 10
     delays = await wait_n(n, max_delay)
     print("Delays:", delays)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
