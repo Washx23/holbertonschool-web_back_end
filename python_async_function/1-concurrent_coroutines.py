9363605bc6517eb3334fca2f7af8baa8f65b91fe
Washx23 <5731@holbertonstudents.com>
2023-08-30 07:02:23 -0700

    1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,15 +4,26 @@
 
 import asyncio
 import random
+from typing import List
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Asynchronous coroutine that waits for a random
+    delay between 0 and max_delay (inclusive) seconds.
+    Returns the waited delay.
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
 
 
-async def wait_n(n, max_delay=10):
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """
+    Asynchronous routine that spawns
+    wait_random n times with specified max_delay.
+    Returns a list of all the delays in ascending order.
+    """
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
     delays.sort()
@@ -20,11 +31,11 @@ async def wait_n(n, max_delay=10):
 
 
 async def main():
+    """module"""
     n = 5
     max_delay = 10
     delays = await wait_n(n, max_delay)
     print("Delays:", delays)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
