dac72c18f083480291233d864bbcf1aac656193f
Washx23 <5731@holbertonstudens.com>
2023-09-03 17:35:54 -0700

    4-tasks.py

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""doc"""
+"""documentation for task 4"""
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
-    """module document"""
+    """module document Everything is documented"""
     delay_list = []
     for _ in range(n):
         delay = await task_wait_random(max_delay)
